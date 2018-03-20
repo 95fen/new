262334c9c82c04c5190963066a98b2e110fb6288
dengxinqiang <474874326@qq.com>
2018-03-20 10:49:44 +0800

    sgtest

--- BG_guanka.py
+++ BG_guanka.py
@@ -1,48 +1,49 @@
-#八挂阵
-import pyautogui
 import time
-from Okbtn import OKstar
-from PIL.ImageOps import grayscale
+import pyautogui
 
-print(pyautogui.getWindows())
-win=pyautogui.getWindow('MNSG.exe - Build 2.05.25.0804')
+
+#《小小军团合战三国PC》.exe
+#win=pyautogui.getWindow('MNSG.exe') #测试PC
+win=pyautogui.getWindow('《小小军团合战三国PC》.exe')  #报审
 print(win.get_position())
 x=win.get_position()[0]
 y=win.get_position()[1]
-#564,63
-#点击【战】按钮
-def zhan():
-   click_zhan= pyautogui.moveTo(x+564,y+63)
-   pyautogui.click(click_zhan)
-def click_btn_Act():#点击攻击按钮
-    pyautogui.moveTo(x + 1085, y + 607)
-    pyautogui.click(x + 1085, y + 607)
-#887,329
-def click_pz():
-    pzbtn=pyautogui.moveTo(x+887,y+329)
-    pyautogui.click(pzbtn)
-#494,601
-def click_ok():
-    ok=pyautogui.moveTo(x+494,y+601)
-    pyautogui.click(ok)
+i=1;
+#破阵按钮886，333
+# 战按钮1082,610
+#结束快捷键563，64
+#确定按钮477，603
+#体力按钮728,399
 
+tlx=x+728
+tly=y+399
+zx=x+1082
+zy=y+610
+jsx=x+563
+jsy=y+64
+okx=477+x
+oky=603+y
+stop=0;
+while stop<50:
+     pyautogui.moveTo(x+886,y+333);
+     pyautogui.click(x+886,y+333);
+     time.sleep(5)
+     if(stop%4==0):
+         pyautogui.moveTo(zx,zy)
+         pyautogui.click(zx,zy)
+         time.sleep(3)
+         pyautogui.moveTo(tlx, tly)
+         pyautogui.click(tlx, tly)
+     else:
+          pyautogui.moveTo(zx, zy)
+          pyautogui.click(zx, zy)
 
-#PzBtn=pyautogui.locateOnScreen(r'C:\Users\dengxinqiang\Desktop\picture\pz.png',grayscale=True)
-#pzbtn=pyautogui.locateCenterOnScreen(r'C:\Users\dengxinqiang\Desktop\picture\pz.png',grayscale=True)
-#btn=pyautogui.moveTo(pzbtn)
-for i in range(20):
-    click_pz()
-    time.sleep(3)
-    click_btn_Act()
-    time.sleep(15)
-    zhan()
-    time.sleep(10)
-    click_ok()
-    time.sleep(8)
-    print(i)
-
-
-
-
+     time.sleep(15)
+     pyautogui.click(jsx,jsy)
+     time.sleep(6)
+     pyautogui.click(okx,oky)
+     time.sleep(6)
+     stop+=1
+     print("第",stop,"次")
 
 
